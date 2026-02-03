da690e040c18dd84aa18bb8d4672d435686916b2
himanshu-ydv01 <himanshu44ydv@gmail.com>
2026-02-03 23:17:40 +0530

    Add OCP & SRP

---
+++ src/Lesson1_SingleResponsibilityPrinciple/Example3/BetterCode/ActivityLogger.java
@@ -0,0 +1,9 @@
+package Lesson1_SingleResponsibilityPrinciple.Example3.BetterCode;
+
+public class ActivityLogger {
+
+    public void logUserActivity(String username) {
+
+    }
+
+}
---
+++ src/Lesson2_OpenClosedPrinciple/Example2/BetterCode/ReportGenerator.java
@@ -0,0 +1,6 @@
+package Lesson2_OpenClosedPrinciple.Example2.BetterCode;
+
+public interface ReportGenerator {
+    String generate();
+
+}
